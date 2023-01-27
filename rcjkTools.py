4b78ec1dfd49127b55bb8e63581188afc5d93c98
Behdad Esfahbod <behdad@behdad.org>
2023-01-27 01:57:11 -0700

    Add decompose.py rcjkTools.py

---
+++ rcjkTools.py
@@ -0,0 +1,20 @@
+def tuplifyLocation(loc):
+    return tuple(sorted(loc.items()))
+
+def dictifyLocation(loc):
+    return {k:v for k,v in loc}
+
+
+def glyphMasters(glyph):
+
+    layersByName = {}
+    for layer in glyph.layers:
+        layersByName[layer.name] = layer
+
+    masters = {}
+    for source in glyph.sources:
+        locationTuple = tuplifyLocation(source.location)
+        masters[locationTuple] = layersByName[source.layerName]
+
+    return masters
+
