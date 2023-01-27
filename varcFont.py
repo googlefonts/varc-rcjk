a102e4a4ff670a6259615f27229c99a2f5eff115
Behdad Esfahbod <behdad@behdad.org>
2023-01-27 01:57:11 -0700

    Add packComponentRecord

--- varcFont.py
+++ varcFont.py
@@ -56,6 +56,88 @@ def setupFvarAxes(rcjkfont, glyphs):
 
     return fvarAxes
 
+
+async def packComponentRecord(rcjkfont, component, coordinateVaries, coordinateHave, transformHave, reverseGlyphMap):
+
+    componentGlyph = await rcjkfont.getGlyph(component.name)
+
+    componentAxes = {axis.name:(axis.minValue,axis.defaultValue,axis.maxValue)
+                     for axis in componentGlyph.axes}
+    coords = component.location
+    coords = normalizeLocation(coords, componentAxes)
+
+    t = component.transformation
+
+    flag = 0
+
+    numAxes = struct.pack(">B", len(coordinateHave))
+
+    gid = reverseGlyphMap[component.name]
+    if gid <= 65535:
+        # gid16
+        gid = struct.pack(">H", gid)
+    else:
+        # gid24
+        gid = struct.pack(">L", gid)[1:]
+        flag |= 1<<12
+
+    axisIndices = []
+    for i,coord in enumerate(coords):
+        if i not in coordinateHave: continue
+        name = '%4d' % i if coord not in fvarTags else coord
+        axisIndices.append(fvarTags.index(name))
+
+    if coordinateVaries:
+        flag |= 1<<13
+
+    if all(v <= 255 for v in axisIndices):
+        axisIndices = b''.join(struct.pack(">B", v) for v in axisIndices)
+    else:
+        axisIndices = b''.join(struct.pack(">H", v) for v in axisIndices)
+        flag |= (1<<1)
+
+    axisValues = b''.join(struct.pack(">h", fl2fi(v, 14)) for i,v in enumerate(coords.values()) if i in coordinateHave)
+
+    c = transformHave
+
+    translateX = translateY = rotation = scaleX = scaleY = skewX = skewY = tcenterX = tcenterY = b""
+    if c.have_translateX:
+        translateX = struct.pack(">h", otRound(t.translateX))
+        flag |= (1<<3)
+    if c.have_translateY:
+        translateY = struct.pack(">h", otRound(t.translateY))
+        flag |= (1<<4)
+    if c.have_rotation:
+        rotation = struct.pack(">h", fl2fi(t.rotation / 180., 12))
+        flag |= (1<<5)
+    if c.have_scaleX:
+        scaleX = struct.pack(">h", fl2fi(t.scaleX, 10))
+        flag |= (1<<6)
+    if c.have_scaleY:
+        scaleY = struct.pack(">h", fl2fi(t.scaleY, 10))
+        flag |= (1<<7)
+    if c.have_skewX:
+        skewX = struct.pack(">h", fl2fi(t.skewX / 180., 14))
+        flag |= (1<<8)
+    if c.have_skewY:
+        skewY = struct.pack(">h", fl2fi(t.skewY / 180., 14))
+        flag |= (1<<9)
+    if c.have_tcenterX:
+        tcenterX = struct.pack(">h", otRound(t.tCenterX))
+        flag |= (1<<10)
+    if c.have_tcenterY:
+        tcenterY = struct.pack(">h", otRound(t.tCenterY))
+        flag |= (1<<11)
+
+    transform = translateX + translateY + rotation + scaleX + scaleY + skewX + skewY + tcenterX + tcenterY
+
+    flag = struct.pack(">H", flag)
+
+    rec = flag + numAxes + gid + axisIndices + axisValues + transform
+
+    return rec
+
+
 async def buildVarcFont(rcjkfont, glyphs):
 
     print("Building varc.ttf")
@@ -146,81 +228,12 @@ async def buildVarcFont(rcjkfont, glyphs):
 
         layer = next(iter(glyph_masters.values()))
         for ci,component in enumerate(layer.glyph.components):
-            componentGlyph = await rcjkfont.getGlyph(component.name)
-            componentAxes = {axis.name:(axis.minValue,axis.defaultValue,axis.maxValue)
-                             for axis in componentGlyph.axes}
-            coords = component.location
-            coords = normalizeLocation(coords, componentAxes)
-
-            t = component.transformation
-
-            flag = 0
-
-            numAxes = struct.pack(">B", len(coordinateHave[ci]))
-
-            gid = reverseGlyphMap[component.name]
-            if gid <= 65535:
-                # gid16
-                gid = struct.pack(">H", gid)
-            else:
-                # gid24
-                gid = struct.pack(">L", gid)[1:]
-                flag |= 1<<12
-
-            axisIndices = []
-            for i,coord in enumerate(coords):
-                if i not in coordinateHave[ci]: continue
-                name = '%4d' % i if coord not in fvarTags else coord
-                axisIndices.append(fvarTags.index(name))
-
-            if coordinateVaries[ci]:
-                flag |= 1<<13
-
-            if all(v <= 255 for v in axisIndices):
-                axisIndices = b''.join(struct.pack(">B", v) for v in axisIndices)
-            else:
-                axisIndices = b''.join(struct.pack(">H", v) for v in axisIndices)
-                flag |= (1<<1)
-
-            axisValues = b''.join(struct.pack(">h", fl2fi(v, 14)) for i,v in enumerate(coords.values()) if i in coordinateHave[ci])
-
-            c = transformHave[ci]
-
-            translateX = translateY = rotation = scaleX = scaleY = skewX = skewY = tcenterX = tcenterY = b""
-            if c.have_translateX:
-                translateX = struct.pack(">h", otRound(t.translateX))
-                flag |= (1<<3)
-            if c.have_translateY:
-                translateY = struct.pack(">h", otRound(t.translateY))
-                flag |= (1<<4)
-            if c.have_rotation:
-                rotation = struct.pack(">h", fl2fi(t.rotation / 180., 12))
-                flag |= (1<<5)
-            if c.have_scaleX:
-                scaleX = struct.pack(">h", fl2fi(t.scaleX, 10))
-                flag |= (1<<6)
-            if c.have_scaleY:
-                scaleY = struct.pack(">h", fl2fi(t.scaleY, 10))
-                flag |= (1<<7)
-            if c.have_skewX:
-                skewX = struct.pack(">h", fl2fi(t.skewX / 180., 14))
-                flag |= (1<<8)
-            if c.have_skewY:
-                skewY = struct.pack(">h", fl2fi(t.skewY / 180., 14))
-                flag |= (1<<9)
-            if c.have_tcenterX:
-                tcenterX = struct.pack(">h", otRound(t.tCenterX))
-                flag |= (1<<10)
-            if c.have_tcenterY:
-                tcenterY = struct.pack(">h", otRound(t.tCenterY))
-                flag |= (1<<11)
-
-            transform = translateX + translateY + rotation + scaleX + scaleY + skewX + skewY + tcenterX + tcenterY
-
-            flag = struct.pack(">H", flag)
-
-            rec = flag + numAxes + gid + axisIndices + axisValues + transform
-
+            rec = await packComponentRecord(rcjkfont,
+                                            component,
+                                            coordinateVaries[ci],
+                                            coordinateHave[ci],
+                                            transformHave[ci],
+                                            reverseGlyphMap)
             data.extend(rec)
 
         ttGlyph = Glyph()
