f2251fe833d3a903519d00c953f72f90655316af
Behdad Esfahbod <behdad@behdad.org>
2023-01-27 01:57:11 -0700

    Add font.py

---
+++ font.py
@@ -0,0 +1,43 @@
+
+from fontTools.fontBuilder import FontBuilder
+
+async def createFontBuilder(rcjkfont, family_name, style, glyphs):
+    upem = await rcjkfont.getUnitsPerEm()
+
+    glyphOrder = ['.notdef'] + list(glyphs.keys())
+    revCmap = await rcjkfont.getReverseCmap()
+    cmap = {}
+    for glyph in glyphs.values():
+        for unicode in revCmap[glyph.name]:
+            # Font has duplicate Unicodes unfortunately :(
+            #assert unicode not in cmap, (hex(unicode), glyphname, cmap[unicode])
+            cmap[unicode] = glyph.name
+
+    metrics = {'.notdef': (upem, 0)}
+    for glyphname in glyphOrder[1:]:
+        glyph = await rcjkfont.getGlyph(glyphname)
+        assert glyph.sources[0].name == "<default>"
+        assert glyph.sources[0].layerName == "foreground"
+        assert glyph.layers[0].name == "foreground"
+        advance = glyph.layers[0].glyph.xAdvance
+        metrics[glyphname] = (max(advance,0), 0) # TODO lsb
+
+    nameStrings = dict(
+        familyName=dict(en=family_name),
+        styleName=dict(en=style),
+    )
+
+    fb = FontBuilder(upem, isTTF=True)
+    #fb.setupHead(unitsPerEm=upem, created=rcjkfont.created, modified=rcjkfont.modified)
+    fb.setupNameTable(nameStrings)
+    fb.setupGlyphOrder(glyphOrder)
+    fb.setupCharacterMap(cmap)
+    fb.setupHorizontalMetrics(metrics)
+    ascent = int(upem * .8) # TODO
+    descent = int(upem * .2) # TODO
+    fb.setupHorizontalHeader(ascent=ascent, descent=descent)
+    #fb.setupOS2(sTypoAscender=os2.sTypoAscender, usWinAscent=os2.usWinAscent, usWinDescent=os2.usWinDescent)
+    fb.setupPost(keepGlyphNames=False)
+
+    return fb
+
--- varc-rcjk.py
+++ varc-rcjk.py
@@ -1,13 +1,9 @@
 # pip install git+https://github.com/BlackFoundryCom/fontra.git
 # pip install git+https://github.com/BlackFoundryCom/fontra-rcjk.git
 
-# See src/fontra/core/classes.py in the fontra repo for the data structure
-# PackedPath objects have a drawPoints method that takes a point pen
-
-
+from font import createFontBuilder
 from fontTools.misc.roundTools import otRound
 from fontTools.ttLib.tables._g_l_y_f import Glyph, GlyphCoordinates
-from fontTools.fontBuilder import FontBuilder
 from fontTools.pens.cu2quPen import Cu2QuMultiPen
 from fontTools.pens.ttGlyphPen import TTGlyphPen
 from fontTools.pens.recordingPen import RecordingPen, RecordingPointPen
@@ -28,46 +24,6 @@ import operator
 import sys
 from fontra_rcjk.backend_fs import RCJKBackend
 
-
-async def createFontBuilder(rcjkfont, family_name, style, glyphs):
-    upem = await rcjkfont.getUnitsPerEm()
-
-    glyphOrder = ['.notdef'] + list(glyphs.keys())
-    revCmap = await rcjkfont.getReverseCmap()
-    cmap = {}
-    for glyph in glyphs.values():
-        for unicode in revCmap[glyph.name]:
-            # Font has duplicate Unicodes unfortunately :(
-            #assert unicode not in cmap, (hex(unicode), glyphname, cmap[unicode])
-            cmap[unicode] = glyph.name
-
-    metrics = {'.notdef': (upem, 0)}
-    for glyphname in glyphOrder[1:]:
-        glyph = await rcjkfont.getGlyph(glyphname)
-        assert glyph.sources[0].name == "<default>"
-        assert glyph.sources[0].layerName == "foreground"
-        assert glyph.layers[0].name == "foreground"
-        advance = glyph.layers[0].glyph.xAdvance
-        metrics[glyphname] = (max(advance,0), 0) # TODO lsb
-
-    nameStrings = dict(
-        familyName=dict(en=family_name),
-        styleName=dict(en=style),
-    )
-
-    fb = FontBuilder(upem, isTTF=True)
-    #fb.setupHead(unitsPerEm=upem, created=rcjkfont.created, modified=rcjkfont.modified)
-    fb.setupNameTable(nameStrings)
-    fb.setupGlyphOrder(glyphOrder)
-    fb.setupCharacterMap(cmap)
-    fb.setupHorizontalMetrics(metrics)
-    ascent = int(upem * .8) # TODO
-    descent = int(upem * .2) # TODO
-    fb.setupHorizontalHeader(ascent=ascent, descent=descent)
-    #fb.setupOS2(sTypoAscender=os2.sTypoAscender, usWinAscent=os2.usWinAscent, usWinDescent=os2.usWinDescent)
-    fb.setupPost(keepGlyphNames=False)
-
-    return fb
 
 def tuplifyLocation(loc):
     return tuple(sorted(loc.items()))
